3edf3ee9d3de36393ed307473b02500fdd5d124d
Oliver Mistarz <oliver.mistarz@outlook.de>
2023-01-15 23:10:39 +0100

    Removed unused ClientToScreen function.

--- MouseToJoystickHandler.cs
+++ MouseToJoystickHandler.cs
@@ -14,9 +14,6 @@ namespace MouseToJoystick2
         [DllImport("User32.Dll")]
         public static extern long SetCursorPos(int x, int y);
 
-        [DllImport("User32.Dll")]
-        public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
-
         [StructLayout(LayoutKind.Sequential)]
         public struct POINT
         {
